c8578d374219f459cb0c766f52c7c95baa4f2fdc
John Doe <johndoe@example.com>
2021-05-05 15:53:26 +0300

    hello

---
+++ _python/python_fundamentals/For Loops Basic I/For.py
@@ -0,0 +1,15 @@
+# basic
+
+# for x in range(1, 151, 1):
+#     print(x)
+
+# Multiples of Five
+# for y in range(5, 1005, 5):
+#     print(y)
+
+
+# Counting, the Dojo Way
+for x in range(1, 151):
+    if x%5==0:
+        print("coding")
+    
