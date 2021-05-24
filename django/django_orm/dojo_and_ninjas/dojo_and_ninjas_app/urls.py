046fff72f17f4ca24af7d2873668facd9c691ddf
mm <laythirshaid1998@gmail.com>
2021-05-24 14:55:29 +0300

    updat

--- django/django_orm/dojo_and_ninjas/dojo_and_ninjas_app/urls.py
+++ django/django_orm/dojo_and_ninjas/dojo_and_ninjas_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('show', views.srap),
+    path('show2', views.secand)
 ]
--- django/django_orm/dojo_and_ninjas/dojo_and_ninjas_app/views.py
+++ django/django_orm/dojo_and_ninjas/dojo_and_ninjas_app/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from dojo_and_ninjas_app.models import dojo, ninja
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def index(request):
-    pass
+    context = {
+        "dojos": dojo.objects.all(),
+    }
+    return render(request , 'index.html',context)
+
+def srap(request):
+    if request.method == 'POST':
+        dojo.objects.create(name=request.POST["name"],city=request.POST["city"],state=request.POST["state"])
+    return redirect('/')
+def secand(request):
+    ninja.objects.create(first_name=request.POST["first_name"],last_name=request.POST["last_name"],dojo_id= dojo.objects.get(id= int(request.POST["dojo_id"])))
+    return redirect('/')
